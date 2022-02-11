9d3e026d1a359fdd13a9e145771f610f07c77e0b
Ho3ein <hos.kh93@gmail.com>
2022-02-11 13:10:39 +0330

    fetch country list from api, store it in state

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CustomInput from "../custom-input/custom-input.component";
 
 import "./search.styles.css";
 
 const Search = () => {
-    return <div className="search-container">
-        <CustomInput />
-    </div>;
+    const [countryList, setCountryList] = useState([]);
+
+    // execute only one time to fetch country list
+    useEffect(() => {
+        fetch(`https://covid-193.p.rapidapi.com/countries`, {
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+                "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+        })
+            .then((response) => {
+                response.json().then((data) => {
+                    const countries = data.response;
+                    setCountryList(countries);
+                });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, []);
+
+    return (
+        <div className="search-container">
+            <CustomInput />
+        </div>
+    );
 };
 
 export default Search;
