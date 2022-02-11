59be377bd8af07256776814556eed34705c139cc
Ho3ein <hos.kh93@gmail.com>
2022-02-11 13:46:56 +0330

    add functionality for fetch data from api for selected country and date

--- src/components/search/search.component.jsx
+++ src/components/search/search.component.jsx
@@ -14,6 +14,10 @@ const Search = () => {
         new Date().toISOString().split("T")[0]
     );
 
+    // if value of input find in list then this state will set to input value
+    const [selectedCountry, setSelectedCountry] = useState(country);
+    const [result, setResult] = useState(null);    
+
     // execute only one time to fetch country list
     useEffect(() => {
         fetch(`https://covid-193.p.rapidapi.com/countries`, {
@@ -34,6 +38,31 @@ const Search = () => {
             });
     }, []);
 
+    // fetch data for selected country and selected dataRange
+    useEffect(() => {
+        // create a parameter to use in fetch
+        const params = new URLSearchParams({
+            country: selectedCountry,
+            day: dateRange,
+        });
+
+        fetch(`https://covid-193.p.rapidapi.com/history?${params}`, {
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+                "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+        })
+            .then((response) => {
+                response.json().then((data) => {
+                    setResult(data.response[0]);
+                });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, [selectedCountry, dateRange]);
+
     const countryMapList = countryList.map((country, index) => {
         return (
             <option value={country} key={index}>
@@ -45,6 +74,11 @@ const Search = () => {
     const countrySearchHandler = (e) => {
         let country = e.target.value;
         setCountry(country);
+
+        // prevnet rendering with every character input
+        if (countryList.indexOf(country) !== -1) {
+            setSelectedCountry(country);
+        }
     };
 
     const datePickerHandler = (e) => {
@@ -68,7 +102,7 @@ const Search = () => {
                     onInput={datePickerHandler}
                 />
             </div>
-            <SearchResult />
+            <SearchResult data={result} />
         </div>
     );
 };
