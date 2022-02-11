4ff2000c5e1d6b6e6bb3907d8269896212db6320
Ho3ein <hos.kh93@gmail.com>
2022-02-11 13:58:32 +0330

    modify search result to send data to cards and show data properly

--- src/components/search-result/search-result.component.jsx
+++ src/components/search-result/search-result.component.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import Card from "../card/card.component";
 
 import "./search-result.styles.css";
 
-const SearchResult = () => {
-    return <div>Search Result</div>;
+const SearchResult = ({ data }) => {
+    const { continent, country, cases, deaths, tests, population } = data;
+    return (
+        <section>
+            <div className="country-info">
+                <h2>{`${continent} / ${country}`}</h2>
+                <h3>Population: {population}</h3>
+            </div>
+            <div className="card-container">
+                <Card
+                    title="cases"
+                    className="gray"
+                    active={cases.active}
+                    critical={cases.critical}
+                    total={cases.total}
+                />
+
+                <Card
+                    title="deaths"
+                    className="red"
+                    active={deaths.new}
+                    total={deaths.total}
+                />
+
+                <Card title="tests" className="orange" total={tests.total} />
+
+                <Card
+                    title="recovered"
+                    className="green"
+                    recovered={cases.recovered}
+                />
+            </div>
+        </section>
+    );
 };
 
 export default SearchResult;
